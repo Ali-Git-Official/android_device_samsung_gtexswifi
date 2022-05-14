d860b8246d6ada83ad83aa599b2292710dc123a8
Ali-Git-Official <90039932+Ali-Git-Official@users.noreply.github.com>
2022-05-14 11:43:44 -0700

    Create platform.mk

---
+++ platform.mk
@@ -0,0 +1,23 @@
+PLATFORM_PATH := device/samsung/gta4xl-common
+
+# Device uses high-density artwork where available
+PRODUCT_AAPT_CONFIG := normal
+PRODUCT_AAPT_PREF_CONFIG := hdpi
+
+
+PRODUCT_CHARACTERISTICS := tablet
+
+# Dynamic Partitions
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    $(PLATFORM_PATH)
+
+DEVICE_PACKAGE_OVERLAYS += \
+    $(PLATFORM_PATH)/overlay
+
+PRODUCT_ENFORCE_RRO_TARGETS := \
+    framework-res
+
+include $(PLATFORM_PATH)/platform/*.mk
