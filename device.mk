81b406195b9365c0f388be9b07482981860bf8e7
Ali-Git-Official <90039932+Ali-Git-Official@users.noreply.github.com>
2022-05-14 13:47:27 -0700

    Update device.mk

--- device.mk
+++ device.mk
@@ -3,9 +3,6 @@ $(call inherit-product, device/samsung/gtexswifi/platform.mk)
 ### PROPRIETARY VENDOR FILES
 $(call inherit-product, vendor/samsung/gtexswifi/gtexswifi-vendor.mk)
 
-### DALVIK
-$(call inherit-product, frameworks/native/build/tablet-7in-hdpi-1024-dalvik-heap.mk)
-
 DEVICE_PATH := device/samsung/gtexswifi
 
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
