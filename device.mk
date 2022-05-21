55f7800d1a4112ce06aca4127a390c4a6747d55a
Ali-Git-Official <90039932+Ali-Git-Official@users.noreply.github.com>
2022-05-21 00:46:13 -0700

    Delete device.mk

--- device.mk
+++
@@ -1,9 +0,0 @@
-### PLATFORM
-$(call inherit-product, device/samsung/gtexswifi/platform.mk)
-### PROPRIETARY VENDOR FILES
-$(call inherit-product, vendor/samsung/gtexswifi/gtexswifi-vendor.mk)
-
-DEVICE_PATH := device/samsung/gtexswifi
-
-DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
-   
